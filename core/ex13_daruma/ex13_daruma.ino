48fb9de4138fa8bf70e593317a1482cab285700f
Wataru KUNINO <git@bokunimo.net>
2022-05-10 22:12:01 +0900

    クラウド対応

--- core/ex13_daruma/ex13_daruma.ino
+++ core/ex13_daruma/ex13_daruma.ino
@@ -5,12 +5,49 @@ Example 13 daruma for M5Stack ～PIR Sensor ユニットで だるまさんが�
 *******************************************************************************/
 
 #include <M5Stack.h>                            // M5Stack用ライブラリの組み込み
+#include <WiFi.h>                               // ESP32用WiFiライブラリ
+#include <WiFiClientSecure.h>                   // TLS(SSL)通信用ライブラリ
+#include <HTTPClient.h>                         // HTTP通信用ライブラリ
 #define PIN_PIR 22                              // G22にセンサ(人感/ドア)を接続
+#define SSID "1234ABCD"                         // 無線LANアクセスポイントのSSID
+#define PASS "password"                         // パスワード
+#define URL  "https://bokunimo.com/daruma/"     // クラウドサービスのURL
 RTC_DATA_ATTR uint32_t pir_delay = 2000;        // 人感センサの解除遅延時間ms(2秒)
+
+/*******************************************************************************
+SSIDを送信したくない場合は、下記の USER にニックネームなどを入力してください。
+*******************************************************************************/
+
+String USER = "";                               // クラウドへ送信するユーザ名
+
+/*******************************************************************************
+ユーザ名 USER は、半角英文字と数字、8文字までを、2つの"の中に入力してください。
+例） String USER = "wataru";
+********************************************************************************
+ユーザ名 USER は下記の目的でアクセス元の端末を特定するために使用します。
+なるべく実名を避け、ニックネームなどを使用してください。
+（デフォルトでは、SSIDの一部(後部4文字)をユーザ名として利用します）
+クラウド・サーバとの通信にはHTTPSを使用し、ユーザ名などの情報は以下の目的、用途で
+使用します。
+
+【利用目的・用途】
+・送信データの傾向学習用
+・システム・アルゴリズムの改良
+・ランキング表示や対戦時の表示用
+・その他、ネット・サービスの開発用
+
+【ご注意】
+・当方の過失による流出については責任を負いません
+・データ消失については責任を負いません（誤った削除依頼で消失する場合もあります）
+・削除依頼については対応しますが、適正な依頼かどうかは当方の判断に基づきます
+
+同意いただけない場合は、当該クエリ（user=）を削除してください。
+*******************************************************************************/
 
 boolean pir;                                    // 人感センサ状態
 int bar100 = 0;                                 // 前回の棒グラフ値(100分率)
 int barPrev = 0;                                // 前回の棒グラフ値(ピクセル数)
+int prisoners = 0;                              // 鬼に捕まったユーザ数
 
 void dispBar(int level=bar100){                 // 棒グラフを描画する関数
     pir = digitalRead(PIN_PIR);                 // 人感センサ値を取得
@@ -39,27 +76,53 @@ void disp(String filename, String msg=""){      // LCDにJPEGファイルを表�
     barPrev = 0;                                // 棒グラフが消えるのでリセット
 }
 
+int getPrisoners(int n = 0){                    // 鬼に捕まったユーザ数
+    String S = String(URL);                     // HTTPリクエスト用の変数
+    S += "?user=" + USER;                       // ユーザ名のクエリを追加
+    if(n) S += "&n="+ String(n);                // 項目nとその値を追加
+    WiFiClientSecure client;                    // TLS/TCP/IP接続部の実体を生成
+    //client.setCACert(rootCACertificate);        // ルートCA証明書を設定
+    client.setInsecure();                       // サーバ証明書を確認しない
+    HTTPClient https;                           // HTTP接続部の実体を生成
+    https.begin(client, S);                     // 初期化と接続情報の設定
+    int httpCode = https.GET();                 // HTTP接続の開始
+    S = https.getString();                      // 受信結果を変数Sへ代入
+    // Serial.println(httpCode);                // httpCodeをシリアル出力
+    // Serial.println(S);                       // 受信結果をシリアル出力
+    if(httpCode == 200){                        // HTTP接続に成功したとき
+        n = S.substring(S.indexOf("\"n\":")+5).toInt();       // パースn
+    }                                           // パース方法は受信サンプル参照
+    https.end();                                // HTTPクライアントの処理を終了
+    client.stop();                              // TLS(SSL)通信の停止
+    return n;
+}
+
 void setup(){                                   // 起動時に一度だけ実行する関数
     M5.begin();                                 // M5Stack用ライブラリの起動
     pinMode(PIN_PIR,INPUT);                     // センサ接続したポートを入力に
     disp("daruma3","Example 13 Daruma-san");    // 持ち手＋タイトルを表示
     disp("daruma2");                            // 顔を表示
     disp("daruma4");                            // タイトルを表示
+    WiFi.mode(WIFI_STA);                        // 無線LANをSTAモードに設定
+    WiFi.begin(SSID,PASS);                      // 無線LANアクセスポイントへ接続
+    while(WiFi.status() != WL_CONNECTED);       // 接続に成功するまで待つ
     while(digitalRead(PIN_PIR));                // 非検出状態になるまで待つ
-    delay(3000);                                // 3秒間の待ち時間処理
-    disp("daruma3","GAME START");               // ゲーム開始
+    if(USER == "") USER = String(SSID).substring(String(SSID).length()-4);
+    prisoners = getPrisoners();                 // 捕まった人数をサーバから取得
+    disp("daruma3","GAME START n=" + String(prisoners)); // ゲーム開始
     delay(1000);                                // 1秒間の待ち時間処理
 }
 
 void loop(){                                    // 繰り返し実行する関数
     disp("daruma0");                            // 顔を表示
     M5.Lcd.drawCentreString("BREAK",68,224,2);  // 文字列"BREAK"を表示
- 	for(int i=0; i <= 100; i++){                // 棒グラフを増加させる
+    for(int i=0; i <= 100; i++){                // 棒グラフを増加させる
         M5.update();                            // ボタン情報を更新
         dispBar(i);                             // 棒グラフの描画
         if(M5.BtnA.isPressed()){                // Aボタンが押されたとき
             M5.Lcd.fillRect(0,224,128,16,WHITE); // "BREAK"を消去
-            disp("daruma2","Cleared!");         // 顔を表示
+            prisoners=getPrisoners(-prisoners); // 捕まった人数をサーバから減算
+            disp("daruma2","Cleared! n=" + String(prisoners));         // 顔を表示
             end();                              // 終了関数endを実行
             return;                             // loop関数の先頭に戻る
         }
@@ -70,11 +133,12 @@ void loop(){                                    // 繰り返し実行する関�
     M5.Lcd.fillRect(0,224,128,16,WHITE);        // "BREAK"を消去
     disp("daruma2");                            // 顔を表示
     delay(pir_delay);                           // PIRセンサの遅延分の待ち時間
-	for(int i=100; i >= 0; i--){                // 棒グラフを減らす処理
+    for(int i=100; i >= 0; i--){                // 棒グラフを減らす処理
         dispBar(i);                             // 棒グラフの描画
         pir = digitalRead(PIN_PIR);             // 人感センサ値を取得
         if(pir){                                // センサ反応時
-            disp("daruma2","Failed");           // "Failed"を表示
+            disp("daruma2","Failed" + String(prisoners)); // "Failed"を表示
+            prisoners = getPrisoners(1);        // 捕まった人数をサーバに加算
             end();                              // 終了関数endを実行
             return;                             // loop関数の先頭に戻る
         }
@@ -88,3 +152,12 @@ void end(){
         dispBar();                              // 棒グラフの描画
     }while(!M5.BtnC.wasPressed());              // ボタンが押されるまで繰り返す
 }
+
+/*
+受信サンプル：
+{"statusCode": 200, "body": {"status": "ok", "n": 6}}
+
+簡易パーサ(n) indexOf("\"n\":")+5
+        "n": 6
+        01234567890123
+*/
