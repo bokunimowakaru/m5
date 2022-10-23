6fa1cf6461bbac5bf1a1818463f4bc242bde142f
Wataru KUNINO <git@bokunimo.net>
2022-10-23 20:30:50 +0900

    IO修正

--- core/ex08_ir_out/ex08_ir_out.ino
+++ core/ex08_ir_out/ex08_ir_out.ino
@@ -9,6 +9,7 @@ Example 8 : Wi-Fi コンシェルジェ リモコン担当(赤外線リモコン
                                           Copyright (c) 2016-2022 Wataru KUNINO
 *******************************************************************************/
 
+#include <M5Stack.h>                        // M5Stack用ライブラリの組み込み
 #include <WiFi.h>                           // ESP32用WiFiライブラリ
 #include <WiFiUdp.h>                        // UDP通信を行うライブラリ
 #include <WebServer.h>                      // HTTPサーバ用ライブラリ
@@ -37,44 +38,43 @@ int IR_TYPE=AEHA;                           // リモコン方式
 
 void handleRoot(){
     char s[97];                             // 文字列変数を定義 97バイト96文字
-    Serial.println("Connected");            // 接続されたことをシリアル出力表示
-    Serial.println(63);                     // (WS2812)LEDを明るい赤色で点灯
+    M5.Lcd.println("Connected");            // 接続されたことをシリアル出力表示
     if(server.hasArg("TYPE")){              // 引数TYPEが含まれていた時
         IR_TYPE = server.arg("TYPE").toInt(); // 引数TYPEの値をIR_TYPEへ
     }
     if(server.hasArg("IR")){                // 引数IRが含まれていた時
         String rx = server.arg("IR");       // 引数IRの値を取得し変数rxへ代入
-        Serial.println(rx);
+        M5.Lcd.println(rx);
         rx.toCharArray(s,97);
         trUri2txt(s);
-        Serial.println(s);
+        M5.Lcd.println(s);
         D_LEN=ir_txt2data(D,DATA_LEN_MAX,s); // 受信データsをリモコン信号に変換
         ir_send(D,D_LEN,IR_TYPE);
     }
     ir_data2txt(s, 97, D, D_LEN);           // 信号データDを表示文字sに変換
     String tx = getHtml(s,D_LEN,IR_TYPE);   // HTMLコンテンツを取得
     server.send(200, "text/html", tx);      // HTMLコンテンツを送信
-    Serial.println(0);                      // (WS2812)LEDを緑色で点灯
 }
 
 void setup(){                               // 起動時に一度だけ実行する関数
     ir_read_init(PIN_IR_IN);                // IRセンサの入力ポートの設定
     ir_send_init(PIN_IR_OUT);               // IR LEDの出力ポートの設定
-    Serial.begin(115200);                   // 動作確認のためのシリアル出力開始
-    Serial.println("ESP32 eg.8 ir_rc");     // タイトルをシリアル出力表示
+    M5.begin();                             // M5Stack用ライブラリの起動
+    M5.Lcd.setBrightness(31);               // 輝度を下げる（省エネ化）
+    M5.Lcd.setTextColor(WHITE);             // 文字色を白(背景なし)に設定
+    M5.Lcd.print("M5 eg.8 ir_rc");          // タイトルをLCDに出力
     WiFi.mode(WIFI_STA);                    // 無線LANをSTAモードに設定
     WiFi.begin(SSID,PASS);                  // 無線LANアクセスポイントへ接続
     while(WiFi.status() != WL_CONNECTED){   // 接続に成功するまで待つ
-        Serial.print('.');                  // (WS2812)LEDの点滅
+        M5.Lcd.print(".");                  // 「.」をLCD表示
         delay(500);                         // 待ち時間処理
     }
     server.on("/", handleRoot);             // HTTP接続時のコールバック先を設定
     server.begin();                         // Web サーバを起動する
-    Serial.println(WiFi.localIP());         // 本機のIPアドレスをシリアル表示
+    M5.Lcd.println(WiFi.localIP());         // 本機のIPアドレスをシリアル表示
     IP_BROAD = WiFi.localIP();              // IPアドレスを取得
     IP_BROAD[3] = 255;                      // ブロードキャストアドレスに
     udp.begin(PORT);                        // UDP通信御開始
-    Serial.println(0);                      // (WS2812)LEDを緑色で点灯
 }
 
 void loop(){
@@ -87,19 +87,17 @@ void loop(){
     /* 赤外線受信・UDP送信処理 */
     d_len=ir_read(d,DATA_LEN_MAX,255);      // 赤外線信号を読み取る
     if(d_len>=16){                          // 16ビット以上の時に以下を実行
-        Serial.println(20);                 // (WS2812)LEDを赤色で点灯
         udp.beginPacket(IP_BROAD, PORT);    // UDP送信先を設定
         udp.print(DEVICE);                  // デバイス名を送信
         udp.print(d_len);                   // 信号長を送信
         udp.print(",");                     // カンマ「,」を送信
         ir_data2txt(s,96,d,d_len);          // 受信データをテキスト文字に変換
         udp.println(s);                     // それを文字をUDP送信
-        Serial.println(s);
+        M5.Lcd.println(s);
         udp.endPacket();                    // UDP送信の終了(実際に送信する)
         memcpy(D,d,DATA_LEN_MAX);           // データ変数dを変数Dにコピーする
         D_LEN=d_len;                        // データ長d_lenをD_LENにコピーする
         delay(500);
-        Serial.println(0);                  // (WS2812)LEDを緑色で点灯
     }
     /*
     d_len=udp.parsePacket();                // UDP受信長を変数d_lenに代入
