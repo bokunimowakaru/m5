df097e79f265a3741d680170e7f6d2f802f6b9f4
Wataru KUNINO <git@bokunimo.net>
2022-11-05 18:52:08 +0900

    ボタン操作・繰り返し撮影

--- core/ex10_cam/ex10_cam.ino
+++ core/ex10_cam/ex10_cam.ino
@@ -40,6 +40,7 @@ IPAddress   IP_BROAD;                       // ブロードキャストIPアド�
 int size=0;                                 // 画像データの大きさ(バイト)
 int update=60;                              // ブラウザのページ更新間隔(秒)
 int cam_size=1;                             // 撮影サイズ 0:VGA 1:QVGA 2:QQVGA
+int shot = 1;                               // 待機時のJPEGカメラ画像の更新
 
 void sendUdp(String dev, String S){
     WiFiUDP udp;                            // UDP通信用のインスタンスを定義
@@ -54,10 +55,24 @@ void sendUdp_Fd(uint16_t fd_num){
     sendUdp(DEVICE,String(fd_num)+", http://"+IP_LOCAL.toString()+"/cam.jpg");
 }
 
+void lcd_update(int shot = 0){
+    M5.Lcd.setCursor(0, 0);                 // 文字表示位置を原点(左上)に
+    M5.Lcd.print("M5Stack eg.10 cam ");     // タイトルを表示
+    if(WiFi.status() == WL_CONNECTED){
+        M5.Lcd.println(WiFi.localIP());
+    }else M5.Lcd.println(WiFi.status());
+    M5.Lcd.setTextColor(WHITE, BLUE);
+    M5.Lcd.drawCentreString(" OneShot ", 65, 28*8, 2);
+    M5.Lcd.drawCentreString(" Stop ", 160, 28*8, 2);
+    if(shot > 1) M5.Lcd.setTextColor(WHITE, RED);
+    M5.Lcd.drawCentreString(" Play ", 255, 28*8, 2);
+    M5.Lcd.setTextColor(WHITE, BLACK);
+}
+
 void setup(){
     M5.begin();                             // M5Stack用ライブラリの起動
     M5.Lcd.setBrightness(31);               // 輝度を下げる（省エネ化）
-    M5.Lcd.println("M5Stack eg.10 cam ");   // タイトルを表示
+    lcd_update();                           // タイトルを表示
     WiFi.mode(WIFI_STA);                    // 無線LANをSTAモードに設定
     WiFi.begin(SSID,PASS);                  // 無線LANアクセスポイントへ接続
     serial2.begin(115200, SERIAL_8N1, PIN_SS2_RX, PIN_SS2_TX); // シリアル初期化
@@ -76,21 +91,20 @@ void setup(){
     IP_BROAD = IP_LOCAL;
     IP_BROAD[3] = 255;
     server.begin();                         // サーバを起動する
-    M5.Lcd.println(IP_LOCAL);               // 本機のIPアドレスをシリアル表示
     sendUdp_Fd(0);                          // 起動したことを知らせるUDP送信
 }
 
 void loop(){
     WiFiClient client;                      // Wi-Fiクライアントの定義
-    char c;                                 // 文字変数を定義
-    char s[65];                             // 文字列変数を定義 65バイト64文字
-    int len=0;                              // 文字列等の長さカウント用の変数
-    int t=0;                                // 待ち受け時間のカウント用の変数
-    int i,j;
-    uint8_t *buf;                           // JPEGデータ用
-
+    uint8_t *buf;                           // JPEGデータ保存用
     buf = (uint8_t *)malloc(JPEG_MAX);      // メモリ確保
-    do{ if(buf){                            // 待機中のカメラ画像表示
+    do{
+        M5.update();                        // ボタン状態の取得
+        delay(1);                           // ボタンの誤作動防止
+        if(M5.BtnA.wasPressed()) shot = 1;  // 1枚だけ撮影
+        if(M5.BtnB.wasPressed()) shot = 0;  // 撮影の停止
+        if(M5.BtnC.wasPressed()) shot = 2;  // 連続撮影
+        if(buf && shot){                    // 以下、待機中のカメラ画像表示
             size=CamCapture();              // カメラで写真を撮影する
             if(size <= JPEG_MAX){           // メモリ容量の確認
                 jpeg_div_t div = JPEG_DIV_NONE;
@@ -99,10 +113,15 @@ void loop(){
                 M5.Lcd.drawJpg(buf,size,0,0,0,0,0,0,div); // JPEGデータ表示
             }
         }
+        lcd_update(shot);                   // タイトルを表示
+        if(shot == 1) shot = 0;             // 1枚だけ撮影時に次回の撮影を停止
         client = server.available();        // 接続されたクライアントを生成
     }while(!client);                        // 未接続時は、doループを繰り返す
     free(buf);                              // JPEGデータ用メモリの開放
-    M5.Lcd.setCursor(0, 0);                 // 文字表示位置を原点(左上)に
+
+    char c;                                 // 文字変数を定義
+    char s[65];                             // 文字列変数を定義 65バイト64文字
+    int len=0, t=0, i, j;                   // HTTPクエリの待ち受け用の変数
     M5.Lcd.println("Connected");            // シリアル出力表示
     while(client.connected()){              // 当該クライアントの接続状態を確認
         if(client.available()){             // クライアントからのデータを確認
@@ -123,8 +142,7 @@ void loop(){
     }
     delay(1);                               // クライアント側の応答待ち時間
     if(!client.connected()||len<6) return;  // 切断された場合はloop()の先頭へ
-    M5.Lcd.println(s);                      // 受信した命令をシリアル出力表示
-    M5.Lcd.println(&s[5]);                  // 受信した命令を液晶に表示
+    M5.Lcd.println(s);                      // 受信した命令を液晶に表示
     if(strncmp(s,"GET / ",6)==0){           // コンテンツ取得命令時
         html(client,size,update,WiFi.localIP()); // コンテンツ表示
         client.flush();                     // ESP32用 ERR_CONNECTION_RESET対策
