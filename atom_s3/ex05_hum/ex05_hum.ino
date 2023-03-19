1b7b6bd33fa62da412d438a0f7fa167fb5ca74a3
Wataru KUNINO <git@bokunimo.net>
2023-03-19 22:35:49 +0900

    文字表示位置の調整

--- atom_s3/ex05_hum/ex05_hum.ino
+++ atom_s3/ex05_hum/ex05_hum.ino
@@ -1,12 +1,17 @@
 /*******************************************************************************
-Example 3: Wi-Fi 照度計 for M5 ATOM S3
-・照度センサ から取得した照度値を送信するIoTセンサです。
+Example 5: Wi-Fi 温湿度計 SENSIRION製 SHT30/SHT31/SHT35 版
+・デジタルI2Cインタフェース搭載センサから取得した温湿度を送信するIoTセンサです。
 
-    使用機材(例)：M5 ATOM S3 + ATOM-HAT(ATOM-MATEに付属) + HAT-DLIGHT
+    使用機材(例)：M5 ATOM S3 + ATOM-HAT(ATOM-MATEに付属) + ENV II/III HAT
 
-                                          Copyright (c) 2021-2023 Wataru KUNINO
+注意: ENV HATのバージョンによって搭載されているセンサが異なります。
+      このプログラムは SHT30 用です。ENV HAT には対応していません。
 
-ボタンでのスリープ解除に未対応です。リセットボタンで解除してください。
+ENV HAT     DHT12 + BMP280 + BMM150 ※非対応
+ENV II HAT  SHT30 + BMP280 + BMM150
+ENV III HAT SHT30 + QMP6988
+
+                                          Copyright (c) 2016-2023 Wataru KUNINO
 *******************************************************************************/
 
 #include "M5AtomS3.h"                           // ATOM S3 用ライブラリ
@@ -15,11 +20,13 @@ Example 3: Wi-Fi 照度計 for M5 ATOM S3
 #include <HTTPClient.h>                         // HTTPクライアント用ライブラリ
 #include "esp_sleep.h"                          // ESP32用Deep Sleep ライブラリ
 
-#define SSID "1234ABCD"                         // 無線LANアクセスポイントSSID
+#define SSID "1234ABCD"                         // 無線LANアクセスポイントのSSID
 #define PASS "password"                         // パスワード
 #define PORT 1024                               // 送信のポート番号
 #define SLEEP_P 30*1000000ul                    // スリープ時間 30秒(uint32_t)
-#define DEVICE "illum_5,"                       // デバイス名(5字+"_"+番号+",")
+#define DEVICE "humid_5,"                       // デバイス名(5字+"_"+番号+",")
+RTC_DATA_ATTR int disp = 0;                     // メータ表示番号 0～
+unsigned long lcd_ms = millis();                // 表示してからの経過時間
 
 /******************************************************************************
  Ambient 設定
@@ -45,23 +52,40 @@ Example 3: Wi-Fi 照度計 for M5 ATOM S3
  *****************************************************************************/
 IPAddress UDPTO_IP = {255,255,255,255};         // UDP宛先 IPアドレス
 
+void disp_init(){ switch(disp){                 // アナログ・メータの初期表示用
+    case 0: analogMeterInit("Celsius", "Temp.", 0, 40); break;
+    case 1: analogMeterInit("RH%", "Humi.", 0, 100); break;
+    default: analogMeterInit("mA", "Batt.I", 0, 160);
+}}
+
 void setup(){                                   // 起動時に一度だけ実行する関数
-    bh1750Setup(6,5);                           // 照度センサの初期化
+    shtSetup(6,5);                              // 湿度センサの初期化
     M5.begin();                                 // M5StickC用Lcdライブラリの起動
 //  M5.Lcd.setBrightness(200);                  // LCDの輝度設定(#if 0で未定義)
     WiFi.mode(WIFI_STA);                        // 無線LANをSTAモードに設定
     WiFi.begin(SSID,PASS);                      // 無線LANアクセスポイントへ接続
-    analogMeterInit("lx", "Illum", 0, 1000);    // アナログ・メータの初期表示
+    disp_init();                                // アナログメータ表示の初期化
 }
 
 void loop(){                                    // 繰り返し実行する関数
     M5.update();                                // ボタン状態の取得
-    float lux = getLux();                       // 照度(lux)を取得
-    if(lux < 0.) sleep();                       // 取得失敗時に末尾のsleepを実行
+    if(M5.Btn.wasPressed()){                    // (過去に)ボタンが押された時
+        disp++;                                 // 画面番号を更新
+        if(disp>1) disp=0;                      // 画面数1超過時に0に戻す
+        disp_init();                            // アナログメータ表示の初期化
+        lcd_ms = millis();                      // スリープ時間の猶予管理用
+    }
+    if(millis()%500) return;                    // 以下は500msに1回だけ実行する
+
+    float temp = getTemp();                     // 温度を取得して変数tempに代入
+    float hum = getHum();                       // 湿度を取得して変数humに代入
+    if(temp < -100. || hum < 0.) sleep();       // 取得失敗時に末尾のsleepを実行
 
 //  if(millis()>3000) M5.Lcd.setBrightness(50); // LCDの輝度設定(#if 0で未定義)
-    analogMeterNeedle(lux,5);                   // 照度に応じてメータ針を設定
-    
+    switch(disp){                               // 画面番号に応じて針を動かす
+        case 0: analogMeterNeedle(temp,5); break;   // 温度メータ
+        case 1: analogMeterNeedle(hum,5); break;    // 湿度メータ
+    }
     M5.Lcd.setTextColor(WHITE,BLACK);           // 文字の色を黒、背景色を白に
     M5.Lcd.setCursor(0,72);                     // 文字の表示位置を原点に設定
     if(WiFi.status() != WL_CONNECTED ){         // Wi-Fi未接続のとき
@@ -69,19 +93,22 @@ void loop(){                                    // 繰り返し実行する関�
         if(millis() > 30000) sleep();           // 30秒超過でスリープ
         return;                                 // loop関数を繰り返す
     }
-    M5.Lcd.println(WiFi.localIP());             // 本機のアドレスをLCDに表示
-    if(M5.Btn.read() || millis()<6000) return;  // 起動後6秒以下はメータ更新
+    M5.Lcd.println(WiFi.localIP());             // 本機のアドレスをシリアル出力
+    if(millis() - lcd_ms < 6000) return;
 
-    String S = String(DEVICE) + String(lux,0);  // 送信データSにデバイス名を代入
+    String S = String(DEVICE);                  // 送信データSにデバイス名を代入
+    S += String(temp,1) + ", ";                 // 変数tempの値を追記
+    S += String(hum,1);                         // 変数humの値を追記
     Serial.println(S);                          // 送信データSをシリアル出力表示
     WiFiUDP udp;                                // UDP通信用のインスタンスを定義
     udp.beginPacket(UDPTO_IP, PORT);            // UDP送信先を設定
     udp.println(S);                             // 送信データSをUDP送信
     udp.endPacket();                            // UDP送信の終了(実際に送信する)
-    if(strcmp(Amb_Id,"00000") == 0) sleep();    // Ambient未設定時にsleepを実行
 
+    if(strcmp(Amb_Id,"00000") == 0) sleep();    // Ambient未設定時にsleepを実行
     S = "{\"writeKey\":\""+String(Amb_Key);     // (項目)writeKey,(値)ライトキー
-    S += "\",\"d1\":\"" + String(lux) + "\"}";  // (項目)d1,(値)照度
+    S += "\",\"d1\":\"" + String(temp,2);       // (項目)d1,(値)温度
+    S += "\",\"d2\":\"" + String(hum,2) + "\"}"; // (項目名)d2,(値)湿度
     HTTPClient http;                            // HTTPリクエスト用インスタンス
     http.setConnectTimeout(15000);              // タイムアウトを15秒に設定する
     String url = "http://ambidata.io/api/v2/channels/"+String(Amb_Id)+"/data";
@@ -97,6 +124,7 @@ void sleep(){                                   // スリープ実行用の関�
     M5.Lcd.println("Sleep...");                 // 「Sleep」をシリアル出力表示
     delay(100);                                 // 送信完了の待ち時間処理
     WiFi.disconnect();                          // Wi-Fiの切断
+    while(M5.Btn.read());                       // ボタン開放待ち
 //  M5.Lcd.setBrightness(0);                    // LCDの輝度設定(#if 0で未定義)
 //  M5.Lcd.fillScreen(BLACK);                   // LCDの消去
 //  M5.Lcd.sleep();                             // LCDの電源オフ(#if 0で未定義)
@@ -104,7 +132,7 @@ void sleep(){                                   // スリープ実行用の関�
     esp_deep_sleep(SLEEP_P);                    // Deep Sleepモードへ移行
 }
 
-/*******************************************************************************
+/******************************************************************************
 【参考文献】
 Arduino IDE 開発環境イントール方法：
 https://docs.m5stack.com/en/quick_start/atoms3/arduino
@@ -112,12 +140,11 @@ https://docs.m5stack.com/en/quick_start/atoms3/arduino
 M5StickC Arduino Library API 情報 (M5StackC 用 ※ATOM S3用ではない )：
 https://docs.m5stack.com/en/api/stickc/system_m5stickc
 
-BH1750FVI データシート 2011.11 - Rev.D (ローム)
-
 https://github.com/m5stack/M5AtomS3
 
 【引用コード】
-https://github.com/bokunimowakaru/esp/tree/master/2_example/example06_lum
-https://github.com/bokunimowakaru/esp/tree/master/2_example/example38_lum
-https://github.com/bokunimowakaru/esp32c3/tree/master/learning/ex03_lum
+https://github.com/bokunimowakaru/esp/tree/master/2_example/example09_hum_sht31
+https://github.com/bokunimowakaru/esp/tree/master/2_example/example41_hum_sht31
+https://github.com/bokunimowakaru/m5s/tree/master/example04d_temp_hum_sht
+https://github.com/bokunimowakaru/esp32c3/tree/master/learning/ex05_hum
 *******************************************************************************/
---
+++ atom_s3/ex05_hum/lib_analogMeterAtomS3.ino
@@ -0,0 +1,312 @@
+/***********************************************************************
+analogMeterInit
+analogMeterNeedle
+
+本ソースコードは下記からダウンロードし、国野亘がロガー用に改変しました。
+https://github.com/m5stack/M5Stack/blob/master/examples/Advanced/Display/TFT_Meter_linear/TFT_Meter_linear.ino
+
+・M5StickC （128 x 128 サイズ
+・M5StickC Plus LCD 240 x 135 (40 x 17) フォント8x6ピクセル
+
+改変部の著作権は国野亘にあります。
+配布は自由ですが権利情報の変更は不可。補償や責任を負いません。
+
+Copyright (c) 2019-2023 Wataru KUNINO
+
+元のライセンスについては下記を参照してください。
+https://github.com/m5stack/M5Stack/blob/master/LICENSE
+
+m5stack/M5Stack is licensed under the MIT License
+
+2019/12/27
+
+以下、TFT_Meter_linear
+*/
+/*
+ An example analogue meter using a ILI9341 TFT LCD screen
+
+ Needs Font 2 (also Font 4 if using large scale label)
+
+ Make sure all the display driver and pin comnenctions are correct by
+ editting the User_Setup.h file in the TFT_eSPI library folder.
+
+ #########################################################################
+ ###### DON'T FORGET TO UPDATE THE User_Setup.h FILE IN THE LIBRARY ######
+ #########################################################################
+ 
+Updated by Bodmer for variable meter size
+ */
+
+// Define meter size as 1 for M5.Lcd.rotation(0) or 1.3333 for M5.Lcd.rotation(1)
+#define M_SIZE 0.5
+
+#define TFT_GREY 0x5AEB
+
+String analogMeterUnit1="Celsius";
+String analogMeterName1="Temp.";
+int analogMeterMinVal1 = 0;
+int analogMeterMaxVal1 = 40;
+
+float ltx1 = 0;    // Saved x coord of bottom of needle
+uint16_t osx1 = M_SIZE*120, osy1 = M_SIZE*120; // Saved x & y coords
+int old_analog1 =  -1; // Value last displayed
+uint32_t updateTime = 0;       // time for next update
+/*
+int value[6] = {0, 0, 0, 0, 0, 0};
+int old_value[6] = { -1, -1, -1, -1, -1, -1};
+int d = 0;
+
+void setup(void) {
+  M5.begin();
+  // M5.Lcd.setRotation(1);
+  // Serial.begin(57600); // For debug
+  M5.Lcd.fillScreen(TFT_BLACK);
+
+  analogMeter(); // Draw analogue meter
+
+  updateTime = millis(); // Next update time
+}
+
+
+void loop() {
+  if (updateTime <= millis()) {
+    updateTime = millis() + 35; // Update emter every 35 milliseconds
+ 
+    // Create a Sine wave for testing
+    d += 4; if (d >= 360) d = 0;
+    value[0] = 50 + 50 * sin((d + 0) * 0.0174532925);
+ 
+    plotNeedle(value[0], 0); // It takes between 2 and 12ms to replot the needle with zero delay
+  }
+}
+*/
+
+// #########################################################################
+//  Draw the analogue meter on the screen
+// #########################################################################
+void analogMeterInit()
+{
+  M5.Lcd.setRotation(2);
+  M5.Lcd.setTextSize(1);
+
+  // Meter outline (0,0) to (319,83) 320x80
+  // M5.Lcd.fillRect(0, 0, 160, 80, TFT_WHITE);
+  // M5.Lcd.fillRect(0, 0, 240, 135, TFT_WHITE);
+  M5.Lcd.fillRect(0, 0, 128, 68, WHITE);
+
+  M5.Lcd.setTextColor(BLACK);  // Text colour
+
+  // Draw ticks every 5 degrees from -50 to +50 degrees (100 deg. FSD swing)
+  for (int i = -50; i < 51; i += 5) {
+    // Long scale tick length
+    int tl = 15;
+
+    // Coodinates of tick to draw
+    float sx = cos((i - 90) * 0.0174532925);
+    float sy = sin((i - 90) * 0.0174532925);
+    uint16_t x0 = sx * (M_SIZE*100 + tl) + M_SIZE*120;
+    uint16_t y0 = sy * (M_SIZE*100 + tl) + M_SIZE*155;
+    uint16_t x1 = sx * M_SIZE*100 + M_SIZE*120;
+    uint16_t y1 = sy * M_SIZE*100 + M_SIZE*155;
+
+    // Coordinates of next tick for zone fill
+    float sx2 = cos((i + 5 - 90) * 0.0174532925);
+    float sy2 = sin((i + 5 - 90) * 0.0174532925);
+    int x2 = sx2 * (M_SIZE*100 + tl) + M_SIZE*120;
+    int y2 = sy2 * (M_SIZE*100 + tl) + M_SIZE*155;
+    int x3 = sx2 * M_SIZE*100 + M_SIZE*120;
+    int y3 = sy2 * M_SIZE*100 + M_SIZE*155;
+
+    // Yellow zone limits
+    //if (i >= -50 && i < 0) {
+    //  M5.Lcd.fillTriangle(x0, y0, x1, y1, x2, y2, TFT_YELLOW);
+    //  M5.Lcd.fillTriangle(x1, y1, x2, y2, x3, y3, TFT_YELLOW);
+    //}
+
+    // Green zone limits
+    if (i >= 0 && i < 25) {
+      M5.Lcd.fillTriangle(x0, y0, x1, y1, x2, y2, GREEN);
+      M5.Lcd.fillTriangle(x1, y1, x2, y2, x3, y3, GREEN);
+    }
+
+    // Orange zone limits
+    if (i >= 25 && i < 50) {
+      M5.Lcd.fillTriangle(x0, y0, x1, y1, x2, y2, ORANGE);
+      M5.Lcd.fillTriangle(x1, y1, x2, y2, x3, y3, ORANGE);
+    }
+
+    // Short scale tick length
+    if (i % 25 != 0) tl = 8;
+
+    // Recalculate coords incase tick lenght changed
+    x0 = sx * (M_SIZE*100 + tl) + M_SIZE*120;
+    y0 = sy * (M_SIZE*100 + tl) + M_SIZE*155;
+    x1 = sx * M_SIZE*100 + M_SIZE*120;
+    y1 = sy * M_SIZE*100 + M_SIZE*155;
+
+    // Draw tick
+    M5.Lcd.drawLine(x0, y0, x1, y1, BLACK);
+
+    // Check if labels should be drawn, with position tweaks
+    if (i % 25 == 0) {
+      // Calculate label positions
+      x0 = sx * (M_SIZE*100 + tl + 10) + M_SIZE*120;
+      y0 = sy * (M_SIZE*100 + tl + 10) + M_SIZE*150;
+      switch (i / 25) {
+        case -2:
+        	M5.Lcd.drawCentreString(String(analogMeterMinVal1), x0+8, y0, 1);
+        	break;
+        case -1:
+        	M5.Lcd.drawCentreString(String((analogMeterMinVal1*3+analogMeterMaxVal1)/4), x0, y0 + 2, 1);
+        	break;
+        case 0:
+        	M5.Lcd.drawCentreString(String((analogMeterMinVal1+analogMeterMaxVal1)/2), x0, y0 +2 , 1);
+        	break;
+        case 1:
+        	M5.Lcd.drawCentreString(String((analogMeterMinVal1+analogMeterMaxVal1*3)/4), x0, y0 + 2, 1);
+        	break;
+        case 2:
+        	M5.Lcd.drawCentreString(String(analogMeterMaxVal1), x0, y0, 1);
+        	break;
+      }
+    }
+
+    // Now draw the arc of the scale
+    sx = cos((i + 5 - 90) * 0.0174532925);
+    sy = sin((i + 5 - 90) * 0.0174532925);
+    x0 = sx * M_SIZE*100 + M_SIZE*120;
+    y0 = sy * M_SIZE*100 + M_SIZE*155;
+    // Draw scale arc, don't draw the last part
+    if (i < 50){
+		M5.Lcd.drawLine(x0, y0, x1, y1, BLACK);
+	}
+  }
+
+
+  M5.Lcd.drawRightString(analogMeterUnit1, M_SIZE*236, M_SIZE*116, 1); // Units at bottom right
+  M5.Lcd.drawCentreString(analogMeterName1, M_SIZE*120, M_SIZE*70, 2); // Comment out to avoid font 4
+
+  analogMeterNeedle(0.0,1); // Put meter needle at 0
+  M5.Lcd.setCursor(0,0);
+}
+
+void analogMeterInit(String unit_S1, String unit_S2, int min_val1, int max_val1){
+  analogMeterUnit1 = unit_S1;
+  analogMeterName1 = unit_S2;
+  analogMeterMinVal1 = min_val1;
+  analogMeterMaxVal1 = max_val1;
+  analogMeterInit();
+}
+void analogMeterInit(String unit_S1, int min_val1, int max_val1){
+  analogMeterUnit1 = unit_S1;
+  analogMeterName1 = unit_S1;
+  analogMeterMinVal1 = min_val1;
+  analogMeterMaxVal1 = max_val1;
+  analogMeterInit();
+}
+
+void analogMeterInit(String unit_S1){
+  analogMeterUnit1 = unit_S1;
+  analogMeterInit();
+}
+
+void analogMeterSetName(String unit_S){
+		if(unit_S.length()>7) analogMeterName1 = unit_S.substring(0, 7);
+		else analogMeterName1 = unit_S;
+}
+
+
+// #########################################################################
+// Update needle position
+// This function is blocking while needle moves, time depends on ms_delay
+// 10ms minimises needle flicker if text is drawn within needle sweep area
+// Smaller values OK if text not in sweep area, zero for instant movement but
+// does not look realistic... (note: 100 increments for full scale deflection)
+// #########################################################################
+void analogMeterNeedle(float value_fn, byte ms_delay)
+{
+  int ch=0;
+  M5.Lcd.setTextColor(BLACK, WHITE);
+  char buf[8];
+  dtostrf(value_fn, 5, 1, buf);
+  M5.Lcd.fillRect(3+ch*160, M_SIZE * 116, M_SIZE * 64, 8, WHITE);
+  M5.Lcd.drawString(buf, M_SIZE*10+ch*160, M_SIZE*(116), 1);
+
+  float value_f=0., delta=0., min=0., ltx=0.;
+  uint16_t osx = M_SIZE*120, osy = M_SIZE*120;
+  String analogMeterName;
+  int old_analog;
+
+	value_f = value_fn;
+	delta = (float)(analogMeterMaxVal1 - analogMeterMinVal1);
+	min = analogMeterMinVal1;
+	ltx = ltx1;
+	osx = osx1;
+	osy = osy1;
+	old_analog = old_analog1;
+	analogMeterName = analogMeterName1;
+  
+  if( delta != 0.){
+	value_f = ( value_f - min ) * 100 / delta;
+  }
+  int value;
+  if(value_f > 0. ) value = (int)(value_f + 0.5);
+  else              value = (int)(value_f - 0.5);
+
+  if (value < -7) value = -7; // Limit value to emulate needle end stops
+  if (value > 107) value = 107;
+
+  // Move the needle until new value reached
+  while (!(value == old_analog)) {
+    if (old_analog < value) old_analog++;
+    else old_analog--;
+
+    if (ms_delay <= 0) old_analog = value; // Update immediately if delay is 0
+
+    float sdeg = map(old_analog, -10, 110, -150, -30); // Map value to angle
+    // Calcualte tip of needle coords
+    float sx = cos(sdeg * 0.0174532925);
+    float sy = sin(sdeg * 0.0174532925);
+
+    // Calculate x delta of needle start (does not start at pivot point)
+    float tx = tan((sdeg + 90) * 0.0174532925);
+
+    // Erase old needle image
+    M5.Lcd.drawLine(M_SIZE*(120 + 20 * ltx - 1) + ch * 160, M_SIZE*(155 - 20), osx - 1 + ch * 160, osy+1, WHITE);
+    M5.Lcd.drawLine(M_SIZE*(120 + 20 * ltx) + ch * 160, M_SIZE*(155 - 20), osx + ch * 160, osy, WHITE);
+    M5.Lcd.drawLine(M_SIZE*(120 + 20 * ltx + 1) + ch * 160, M_SIZE*(155 - 20), osx + 1 + ch * 160, osy+1, WHITE);
+
+    // Re-plot text under needle
+    M5.Lcd.setTextColor(BLACK);
+    M5.Lcd.fillRect(M_SIZE*70 + ch * 160, M_SIZE*70, M_SIZE*100, 16,WHITE);
+    M5.Lcd.drawCentreString(analogMeterName, M_SIZE*120 + ch * 160, M_SIZE*70, 2); // // Comment out to avoid font 4
+
+    // Store new needle end coords for next erase
+    ltx = tx;
+    osx = M_SIZE*(sx * 98 + 120);
+    osy = M_SIZE*(sy * 98 + 155);
+
+    // Draw the needle in the new postion, magenta makes needle a bit bolder
+    // draws 3 lines to thicken needle
+    M5.Lcd.drawLine(M_SIZE*(120 + 20 * ltx - 1) + ch * 160, M_SIZE*(155 - 20), osx - 1 + ch * 160, osy+1, RED);
+    M5.Lcd.drawLine(M_SIZE*(120 + 20 * ltx) + ch * 160, M_SIZE*(155 - 20), osx + ch * 160, osy, MAGENTA);
+    M5.Lcd.drawLine(M_SIZE*(120 + 20 * ltx + 1) + ch * 160, M_SIZE*(155 - 20), osx + 1 + ch * 160, osy+1, RED);
+
+    // Slow needle down slightly as it approaches new postion
+    if (abs(old_analog - value) < 10) ms_delay += ms_delay / 5;
+
+    // Wait before next update
+    delay(ms_delay);
+  }
+
+	ltx1 = ltx;
+	osx1 = osx;
+	osy1 = osy;
+	old_analog1 = old_analog;
+
+  M5.Lcd.setTextColor(WHITE);
+}
+
+void analogMeterNeedle(float value_fn){
+	analogMeterNeedle(value_fn, 35);
+}
